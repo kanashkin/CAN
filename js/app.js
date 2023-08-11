e2508e2f3ad16337410e0b11dc018de93c1ed276
kanashkin <kanashkin_ad@mail.ru>
2023-08-11 18:35:15 +0300

    ready faq block

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const headerTabs = document.querySelectorAll('.header__tab')
-const mainTitlte = document.querySelector('.main__title')
+const headerTabs = document.querySelectorAll('.header__tab'),
+    mainTitlte = document.querySelector('.main__title')
 
 headerTabs.forEach(item => {
     item.addEventListener('click', (e) => {
@@ -20,5 +20,21 @@ tabs.forEach(item => {
             item.classList.remove('card__button-mini-active')
         })
         item.classList.add('card__button-mini-active')
+    })
+})
+
+const collapsBlocks = document.querySelectorAll('.faq__question'),
+    collapsTriggers = document.querySelectorAll('.faq__question-trigger')
+
+collapsTriggers.forEach(item => {
+    item.addEventListener('click', (e) => {
+        collapsTriggers.forEach(item => {
+            item.setAttribute('src', 'img/faq/plus.svg')
+        })
+        collapsBlocks.forEach(item => {
+            item.classList.remove('faq__question-active')
+        })
+        e.target.setAttribute('src', 'img/faq/arrow.svg')
+        e.target.parentNode.classList.add('faq__question-active')
     })
 })
